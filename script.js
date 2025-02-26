dfe60320dcbe4430481e4762e8855ba1565c56a8
Vijeth <vijethshetty334@gmail.com>
2025-02-27 00:33:42 +0530

    Update env File

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-import env from 'dotenv';
-
-env.config();
-const API_KEY = process.env.API_KEY;
-const BASE_URL = process.env.BASE_URL;
+const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8"
+const BASE_URL = "https://api.themoviedb.org/3"
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = BASE_URL + "/search/movie?" + API_KEY;
